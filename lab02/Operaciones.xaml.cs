3a325eccf3a36c4ec82337c53b42b51d0a45e753
RafaelReynoso <rafael.reynoso@tecsup.edu.pe>
2024-03-23 12:53:46 -0500

    commit

--- lab02/Operaciones.xaml.cs
+++ lab02/Operaciones.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,42 +19,14 @@ namespace lab02
     /// </summary>
     public partial class Operaciones : Window
     {
-        public ObservableCollection<Operacion> OperacionesList { get; set; }
         public Operaciones()
         {
             InitializeComponent();
-            OperacionesList = new ObservableCollection<Operacion>();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Insertar(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Datos Guardados");
         }
-
-        public class Operacion
-        {
-            public string TipoDocumento { get; set; }
-            public string NumeroDocumento { get; set; }
-            public string Placa { get; set; }
-            public string Turno { get; set; }
-            public string Conductor { get; set; }
-            public string Cliente { get; set; }
-            public DateTime Fecha { get; set; }
-            public double Peso { get; set; }
-
-            // Constructor
-            public Operacion(string tipoDocumento, string numeroDocumento, string placa, string turno, string conductor, string cliente, DateTime fecha, double peso)
-            {
-                TipoDocumento = tipoDocumento;
-                NumeroDocumento = numeroDocumento;
-                Placa = placa;
-                Turno = turno;
-                Conductor = conductor;
-                Cliente = cliente;
-                Fecha = fecha;
-                Peso = peso;
-            }
-        }
-
     }
 }
